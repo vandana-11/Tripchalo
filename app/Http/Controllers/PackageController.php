b45fe281ba30ee61783415591232b06d817e7814
Dr.Vandana <vb11.vandana8@gmail.com>
2018-09-08 17:11:27 +0530

    Added: Implemented store() method in PackageController

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -1,10 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Package;
 use Illuminate\Http\Request;
 
 class PackageController extends Controller
 {
-    //
+    public function index()
+    {
+        return Package::all();
+    }
+    public function create()
+    {
+        return Package::all();
+    }
+    public function store(Request $request)
+    {
+        $package = new Package;
+        $package->title = $request->title;
+        $package->slug = $request->slug;
+        $package->user_id = $request->user_id;
+        $package->body = $request->body;
+        $package->thumbnail = $request->thumbnail;
+        $package->price = $request->price;
+        $package->discount = $request->discount;
+        $package->is_active = $request->is_active;
+        $package->is_expired = $request->is_expired;
+        $package->is_deleted = $request->is_deleted;
+        $package->save();
+        return $page;
+    }
+
 }
