41637b6503f200e622d04630f60856cb7a23ecca
Dr.Vandana <vb11.vandana8@gmail.com>
2018-09-08 13:06:34 +0530

    Added: Implemented index, create and store methods in PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,10 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Page;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return Page::all();
+    }
+
+     public function create()
+    {
+        return Page::all();
+    }
+
+     public function store(Request $request)
+    {
+        $page = new Page;
+        $page->user_id = $request->user_id;
+        $page->title = $request->title;
+        $page->body = $request->body;
+        $page->slug = $request->slug;
+        $page->order_id = $request->order_id;
+        $page->save();
+        return $page;
+      
+    }
+
 }
