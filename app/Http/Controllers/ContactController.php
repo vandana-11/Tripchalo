5bc838777b27d2010542027367b443b71359d97d
Dr.Vandana <vb11.vandana8@gmail.com>
2018-09-09 18:15:04 +0530

    Added: Implemented store() method in ContactController and PackageController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+   public function index()
+    {
+        return Contact::all();
+    }
+    public function create()
+    {
+        return Contact::all();
+    }
+
+    public function store(Request $request)
+    {
+        $contact = new Contact;
+
+        $contact->email = $request->email;
+        $contact->message = $request->message;
+        $contact->save();
+        return $contact;
+    }
 }
