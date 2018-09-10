b6cd66e45e519a28f8241305612bb99fa88c6c36
Dr.Vandana <vb11.vandana8@gmail.com>
2018-09-10 11:25:35 +0530

    Added:  Implemented index() and store() methods in ContactController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -1,10 +1,76 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    //
+    /**
+     * Returns all data from images table
+     * @return [type] [description]
+     */
+    public function index()
+    {
+        return Image::all();
+    }
+
+      /**
+     * Create a new record into images table
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+
+    public function store(Request $request)
+    {
+        $image = new Image;
+        $image->package_id = $request->package_id;
+        $image->filename = $request->filename;
+        $image->path = $request->path;
+        $image->save();
+        return $image;
+      
+    }
+
+    /**
+     * Return one row based on given id
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public function show($id)
+    {
+        return Page::find($id);
+    }
+
+    /**
+     * Update a given row based on given id
+     * @param  Request $request [description]
+     * @param  [type]  $id      [description]
+     * @return [type]           [description]
+     */
+
+    public function update(Request $request, $id)
+    {
+        $page = Page::find($id);
+        $page->user_id = $request->user_id;
+        $page->title = $request->title;
+        $page->body = $request->body;
+        $page->slug = $request->slug;
+        $page->order_id = $request->order_id;
+        $page->save();
+        return $page;
+
+    }
+
+    /**
+     * Delete a record based on given $id
+     * @param  Package $page [description]
+     * @return [type]        [description]
+     */
+    public function destroy($id)
+    {
+        $page = Page::find($id);
+        return $page->delete() ? 'True' : 'False';
+    }
+
 }
